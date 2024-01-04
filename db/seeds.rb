f456bd080cf0beb06984d42fb5d68790649334e2
abhchand <me@abhchand.me>
2024-01-04 08:44:12 -0500

    Guardrails for running seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
-return if Widget.count > 0
+if !ENV['RACK_ENV'] == 'development'
+  puts "\tSeeding will not run on non-development environments"
+  return
+end
+
+if Widget.count > 0
+  puts "\tData already exists... skipping"
+  return
+end
+
+# ########################################################################
 
 puts "\t=== Seeding Widgets"
 w1 = Widget.create!(name: "Foo")
