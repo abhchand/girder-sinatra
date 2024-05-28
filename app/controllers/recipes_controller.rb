d8eefc6429828d8ed57f69de6167b0c3caa30af7
abhchand <me@abhchand.me>
2024-05-27 23:01:05 -0400

    move `recipe-list` to separate template

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 get "/recipes", auth: :user do
   @recipes = Recipe.all.order(:name)
-  @icon_share = APP_ROOT.join("public", "images", "icons", "share.svg").read
+  @share_icon = APP_ROOT.join("public", "images", "icons", "share.svg").read
 
   erb :"recipes/index"
 end
