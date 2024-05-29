668c705a031dc3527d17b8412da2a2589e47bd37
abhchand <me@abhchand.me>
2024-05-28 23:36:08 -0400

    log error and render partial without layout

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -3,14 +3,15 @@ get "/api/recipes" do
 
   begin
     response = RecipeSearchService.new(params).call
-  rescue
+  rescue => e
     status 500
-    { error: 'an error occurred' }.to_json
+    { error: "an error occurred: #{e.message}" }.to_json
     return
   end
 
   locals = { recipes: response["items"] }
-  html = erb(:"recipes/recipe-item", {}, locals)
+  options = { layout: false }
+  html = erb(:"recipes/recipe-item", options, locals)
 
   status 200
   { html: html }.to_json
